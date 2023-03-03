f14732167fb6f8f3039850f316643080929c86c0
Renato <renato.lage@ligohub.com>
2023-03-03 16:19:50 -0300

    Exercício Área de um Trapézio

---
+++ ATP/Exercicios/AreaTrapezio/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+float baseMaior = 0;
+float baseMenor = 0;
+float altura = 0;
+Console.WriteLine("Digite o valor da base MAIOR: ");
+baseMaior = float.Parse(Console.ReadLine());
+Console.WriteLine("Digite o valor da base MENOR: ");
+baseMenor = float.Parse(Console.ReadLine());
+Console.WriteLine("Digite o valor da Altura: ");
+altura = float.Parse(Console.ReadLine());
+float area = ((baseMaior + baseMenor) * altura) / 2;
+Console.WriteLine("O valor da área do trapézio: " + area );
+
+
+
+
+
