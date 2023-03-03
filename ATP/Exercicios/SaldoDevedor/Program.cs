f13a5ebe9ea3efa8c543c770b7fc253a6083faa4
Renato <renato.lage@ligohub.com>
2023-03-03 16:33:37 -0300

    Exercício: cálculo saldo devedor

---
+++ ATP/Exercicios/SaldoDevedor/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+float numPrest = 0;
+float prestPagas = 0;
+float valorPrest = 0;
+Console.WriteLine("Insira e número total de prestações: ");
+numPrest = float.Parse(Console.ReadLine());
+Console.WriteLine("Quantas prestações já foram pagas? ");
+prestPagas = float.Parse(Console.ReadLine());
+Console.WriteLine("Qual o valor de cada prestação? ");
+valorPrest = float.Parse(Console.ReadLine());
+float saldoDevedor = (numPrest - prestPagas) * valorPrest;
+Console.WriteLine("Saldo devedor é de R$" + saldoDevedor);
+
+
+
+
