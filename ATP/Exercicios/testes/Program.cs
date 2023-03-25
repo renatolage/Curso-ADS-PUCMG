e5c21291102534c197605ec72f8e464622bfefb5
Renato <renato.lage@ligohub.com>
2023-03-25 12:01:57 -0300

    Adição do Trabalho de DIW

--- ATP/Exercicios/testes/Program.cs
+++ ATP/Exercicios/testes/Program.cs
@@ -1,10 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-int numero = 0;
-Console.WriteLine("Digite um número:");
-numero = int.Parse(Console.ReadLine());
-if ((numero >= 1) && (numero <= 3)){
-    Console.WriteLine("Número é " + numero);
-}
+int numero = 10;
+int numero2 = 5;
+Console.WriteLine("Digite um número:" + (numero+numero2));
+
 
 
 
